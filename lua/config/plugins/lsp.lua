09ca6f4762b8639ad78aa394e5fb7fe2e27dab63
Nick Cernis <nick@cern.is>
2023-05-21 11:26:16 +0200

    fix: update mason-null-ls setup

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -340,17 +340,8 @@ return {
         ensure_installed = { "stylua", "eslint_d", "prettierd" },
       })
 
-      require("mason-null-ls").setup_handlers({
-        function(source_name, methods)
-          -- All sources with no handler get passed here.
-
-          -- To keep the original functionality of `automatic_setup = true`,
-          -- please add the below.
-          require("mason-null-ls.automatic_setup")(source_name, methods)
-        end,
-        -- stylua = function(source_name, methods)
-        --   null_ls.register(null_ls.builtins.formatting.stylua)
-        -- end,
+      require("mason-null-ls").setup({
+        automatic_setup = true,
       })
 
       -- Set up installed and configured sources above.
