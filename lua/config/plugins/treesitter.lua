6b696d8ebf40671185bbbff23b90bbee7eabde05
Nick Cernis <nick@cern.is>
2023-05-23 20:53:47 +0200

    chore: reduce treesitter parsers installed by default

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -8,7 +8,11 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         -- A list of parser names, or "all"
-        ensure_installed = "all",
+        -- See https://github.com/nvim-treesitter/nvim-treesitter#supported-languages.
+        -- `:TSUninstall all` to uninstall all.
+        -- `:TSUpdate` to install all from ensure_installed.
+        ensure_installed = { "c", "lua", "javascript", "json", "php", "phpdoc", "rust", "toml", "markdown", "typescript",
+          "tsx", "go", "toml", "yaml", "vim", "vimdoc", "query", "zig" },
         highlight = { enable = true },
         indent = { enable = true, disable = { "python" } },
         incremental_selection = {
