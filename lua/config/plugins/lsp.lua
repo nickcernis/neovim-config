0a4d48b8d2f73c2a3448da4829115302d8c2d6f2
Nick Cernis <nick@cern.is>
2023-05-26 00:13:31 +0200

    feat: confirm completion with tab

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -303,6 +303,7 @@ return {
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
