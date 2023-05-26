d37946e5f43748109afd0e1db96dbebfa878d8d5
Nick Cernis <nick@cern.is>
2023-05-26 23:47:10 +0200

    chore: try tokyonight theme

--- README.md
+++ README.md
@@ -101,6 +101,9 @@ Configs in `./lua/config/plugins/` are automatically loaded.
 
 Things to explore:
 
+- https://github.com/folke/tokyonight.nvim
+- https://github.com/folke/zen-mode.nvim
+
 ### Git
 
 - Explore https://github.com/wfxr/forgit with https://github.com/ray-x/forgit.nvim.
--- lua/config/plugins/appearance.lua
+++ lua/config/plugins/appearance.lua
@@ -1,25 +1,46 @@
 return {
   -- Gruvbox: the best theme if you like brown.
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false, -- Load  during startup.
-    priority = 1000, -- Load before other start plugins.
-    config = function()
-      require("gruvbox").setup({
-        italic = {
-          strings = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        bold = false,
-        contrast = "hard",
-      })
-      vim.cmd("colorscheme gruvbox")
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   lazy = false,    -- Load  during startup.
+  --   priority = 1000, -- Load before other start plugins.
+  --   config = function()
+  --     require("gruvbox").setup({
+  --       italic = {
+  --         strings = false,
+  --         comments = false,
+  --         operators = false,
+  --         folds = false,
+  --       },
+  --       bold = false,
+  --       contrast = "hard",
+  --     })
+  --     vim.cmd("colorscheme gruvbox")
+  --
+  --     -- Theme overrides.
+  --     -- Give the color column less contrast.
+  --     vim.cmd([[highlight ColorColumn guibg=#242729]])
+  --   end,
+  -- },
 
-      -- Theme overrides.
-      -- Give the color column less contrast.
-      vim.cmd([[highlight ColorColumn guibg=#242729]])
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("tokyonight").setup({
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+        },
+        on_colors = function(colors)
+          colors.comment = "#747fb3" -- Original about "#575f86".
+        end
+      })
+      vim.cmd("colorscheme tokyonight-night")
     end,
   },
 
