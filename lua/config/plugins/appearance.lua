35748d606815462c19fda9cd8e0e15d806beef25
Nick Cernis <nick@cern.is>
2024-01-15 21:15:43 +0100

    chore: scrub bad indentline options

--- lua/config/plugins/appearance.lua
+++ lua/config/plugins/appearance.lua
@@ -48,24 +48,9 @@ return {
   -- https://github.com/lukas-reineke/indent-blankline.nvim
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     config = function()
-      -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#363433 gui=nocombine]]
-      require("indent_blankline").setup({
-        char = "┊",
-        show_end_of_line = true,
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-        show_trailing_blankline_indent = false,
-        char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IndentBlanklineIndent5",
-          "IndentBlanklineIndent6",
-        },
-      })
+      require("ibl").setup()
     end,
   },
 }
