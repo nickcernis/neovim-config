84e96cc5bbf723830385f3a87e5cec8ac128068d
Nick Cernis <nick@cern.is>
2026-05-06 16:07:24 +0200

    switch nvim-treesitter to main branch for nvim 0.12 compatibility

--- init.lua
+++ init.lua
@@ -378,6 +378,7 @@ require("lazy").setup({
   -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = function()
       require("nvim-treesitter.install").update({ with_sync = true })
     end,
